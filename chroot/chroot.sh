ed9a3e7d80ede6704c36ad88d5331c468867c5a0
Cristian "DrOptix" Dinu <cristian.virgil.dinu@gmail.com>
2024-01-25 21:37:11 +0200

    feat(chroot): Make `cat` work inside jail
    Provided a `secret.txt` inside `new_root`, you can run:
    ```bash
    cat /secret.txt
    ```
    
    It should print
    ```
    this is a secret
    ```

--- chroot/chroot.sh
+++ chroot/chroot.sh
@@ -8,6 +8,9 @@ mkdir -vp /work/chroot/new_root/bin/
 mkdir -vp /work/chroot/new_root/lib/x86_64-linux-gnu/
 mkdir -vp /work/chroot/new_root/lib64/
 
+# Create the secret file
+echo "this is a secret" > ./new_root/secret.txt
+
 # Copy `bash` and dependencies
 cp -v /bin/bash ./new_root/bin/
 cp -v /lib/x86_64-linux-gnu/libtinfo.so.5 \
@@ -23,6 +26,9 @@ cp -v /lib/x86_64-linux-gnu/libselinux.so.1 \
     /lib/x86_64-linux-gnu/libpcre.so.3 \
     /lib/x86_64-linux-gnu/libpthread.so.0 \
     ./new_root/lib/x86_64-linux-gnu/
+
+# Copy `cat` and dependencies
+cp -v /bin/cat ./new_root/bin/
 
 # Start `bash` jailed to `new_root`
 chroot new_root bash
