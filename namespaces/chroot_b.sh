a0b9eaf459a3d93a2cebef9fac028f7ab8e9bd3c
Cristian "DrOptix" Dinu <cristian.virgil.dinu@gmail.com>
2024-01-26 14:32:45 +0200

    feat(namespaces): POC read secret of `root_a` from inside `root_b` jail

--- namespaces/chroot_b.sh
+++ namespaces/chroot_b.sh
@@ -7,6 +7,7 @@ rm -vrf /work/chroot/root_b/*
 debootstrap --variant=minbase bionic root_b/
 
 cp kill_tail.sh ./root_b/kill_tail.sh
+cp read_secret.sh ./root_b/read_secret.sh
 
 # Enter `root_b` jail
 chroot root_b/ bash
